4ecaf24beb9c1e4e8f44e4937bbacb6207d00309
Susanne Moog <susanne.moog@typo3.com>
2017-01-12 18:59:41 +0100

    [FEATURE] Add new custom form conversion

--- Classes/Service/Form/ConverterService.php
+++ Classes/Service/Form/ConverterService.php
@@ -8,33 +8,80 @@ namespace T3G\Hubspot\Service\Form;
  */
 class ConverterService
 {
+    const STRINGIFY = 'stringify-';
 
     /**
      * @param array $formData
      *
      * @return array
      */
-    public function convertToHubspotFormat(array $formData) : array
+    public function convertToHubspotFormat(array $formData): array
     {
         $hubspotData = [];
         foreach ($formData as $datum) {
+            $parts = [];
             if (isset($datum['hubspotTable'], $datum['hubspotProperty'], $datum['value'])) {
                 $hubspotProperty = $datum['hubspotProperty'];
                 $hubspotTable = $datum['hubspotTable'];
 
                 if (strpos($hubspotProperty, '.') > 0) {
-                    list($key, $property) = explode('.', $hubspotProperty);
+                    $parts = $this->convertToNestedStructure($hubspotProperty, $datum);
                 } else {
-                    $key = 0;
-                    $property = $hubspotProperty;
+                    $parts[$hubspotTable][] = [
+                        'value' => $datum['value'],
+                        'property' => $hubspotProperty,
+                    ];
                 }
-                $hubspotData[$hubspotTable][$key][] = [
-                    'property' => $property,
-                    'value' => $datum['value']
+                $hubspotData = array_merge_recursive($hubspotData, $parts);
+            }
+        }
+        $hubspotData = self::normalizeKeys($hubspotData);
+        return $hubspotData;
+    }
+
+    /**
+     * @param $hubspotProperty
+     * @param $datum
+     * @return array
+     */
+    protected function convertToNestedStructure($hubspotProperty, $datum): array
+    {
+        $parts = explode('.', $hubspotProperty);
+        array_walk(
+            array_reverse($parts), function ($v, $k) use (&$parts, $datum) {
+            if (is_numeric($v)) {
+                $v = self::STRINGIFY . $v;
+            }
+            if (!empty($k)) {
+                $parts = [$v => $parts];
+            } else {
+                $parts = [
+                    [
+                        'property' => $v,
+                        'value' => $datum['value'],
+                    ],
                 ];
             }
         }
+        );
+        return $parts;
+    }
 
-        return $hubspotData;
+    protected static function normalizeKeys(array $input)
+    {
+        $return = [];
+        $len = strlen(self::STRINGIFY);
+        foreach ($input as $key => $value) {
+            if (is_string($key) && strpos($key, self::STRINGIFY) === 0) {
+                $key = (int)substr($key, $len);
+            }
+
+            if (is_array($value)) {
+                $value = self::normalizeKeys($value);
+            }
+
+            $return[$key] = $value;
+        }
+        return $return;
     }
 }
