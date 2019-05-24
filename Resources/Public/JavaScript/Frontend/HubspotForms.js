c0a2ff9a8b3890ae1568d5ec42c1ee22e1b1e16e
Benjamin Kott <benjamin.kott@outlook.com>
2019-05-24 17:43:20 +0200

    [HOTFIX] JavaScript is not initialized on event sites
    See patch that introduced that problem.
    https://bitbucket.typo3.com/projects/EXT/repos/hubspot/commits/ff67427406eaee47895d18d058712b09e3079301

--- Resources/Public/JavaScript/Frontend/HubspotForms.js
+++ Resources/Public/JavaScript/Frontend/HubspotForms.js
@@ -1,14 +1,14 @@
-/**
- * Module: TYPO3/CMS/Hubspot/Frontend/HubspotForms
- */
-define(['//js.hsforms.net/forms/v2.js'], function () {
-    window.addEventListener('load', function () {
-        const hubspotFormContainers = document.querySelectorAll('.t3js-hubspot-form');
-        Array.from(hubspotFormContainers).forEach(function (container) {
-            const disableCss = parseInt(container.dataset.disableCss || 1, 10) === 1;
-            const portalId = container.dataset.portalId;
-            const formId = container.dataset.formId;
+var script = document.createElement("script");
+script.src = '//js.hsforms.net/forms/v2.js';
+document.head.appendChild(script);
 
+window.addEventListener('load', function () {
+    const hubspotFormContainers = document.querySelectorAll('.t3js-hubspot-form');
+    Array.from(hubspotFormContainers).forEach(function (container) {
+        const disableCss = parseInt(container.dataset.disableCss || 1, 10) === 1;
+        const portalId = container.dataset.portalId;
+        const formId = container.dataset.formId;
+        if (portalId && formId) {
             const options = {
                 portalId: portalId,
                 formId: formId,
@@ -18,12 +18,12 @@ define(['//js.hsforms.net/forms/v2.js'], function () {
                     messageElement.parentElement.removeChild(messageElement);
                 }
             };
-
             if (disableCss) {
                 options.css = '';
             }
-
             hbspt.forms.create(options);
-        });
+        } else {
+            console.log('Please validate your HubSpot Portal-ID and Form-ID.')
+        }
     });
 });
