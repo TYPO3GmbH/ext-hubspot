d48f8cb3c13689fc86e2fb3bfbd6cc497ce6b64c
Andreas Fernandez <andreas.fernandez@typo3.com>
2020-08-20 16:06:57 +0200

    [BUGFIX] Add usercentrics consent for Hubspot (#2)

--- Resources/Public/JavaScript/Frontend/HubspotForms.js
+++ Resources/Public/JavaScript/Frontend/HubspotForms.js
@@ -1,29 +1,43 @@
-var script = document.createElement("script");
-script.src = '//js.hsforms.net/forms/v2.js';
-document.head.appendChild(script);
-
-window.addEventListener('load', function () {
-    const hubspotFormContainers = document.querySelectorAll('.t3js-hubspot-form');
-    Array.from(hubspotFormContainers).forEach(function (container) {
-        const disableCss = parseInt(container.dataset.disableCss || 1, 10) === 1;
-        const portalId = container.dataset.portalId;
-        const formId = container.dataset.formId;
-        if (portalId && formId) {
-            const options = {
-                portalId: portalId,
-                formId: formId,
-                target: '#' + container.id,
-                onFormReady: function () {
-                    const messageElement = document.querySelector("#hubspot-form-message");
-                    messageElement.parentElement.removeChild(messageElement);
-                }
-            };
-            if (disableCss) {
-                options.css = '';
-            }
-            hbspt.forms.create(options);
-        } else {
-            console.log('Please validate your HubSpot Portal-ID and Form-ID.')
+(function() {
+    document.addEventListener('readystatechange', function(e) {
+        if (document.readyState === 'interactive') {
+            loadHubspotForms();
         }
     });
-});
+    function loadHubspotForms() {
+        var script = document.createElement("script");
+        script.src = '//js.hsforms.net/forms/v2.js';
+        if (typeof window.usercentrics !== 'undefined') {
+            script.type = 'text/plain';
+            script.dataset.usercentrics = 'HubSpot Forms';
+        }
+        document.head.appendChild(script);
+    }
+
+    window.addEventListener('load', function () {
+        const hubspotFormContainers = document.querySelectorAll('.t3js-hubspot-form');
+        Array.from(hubspotFormContainers).forEach(function (container) {
+            const disableCss = parseInt(container.dataset.disableCss || 1, 10) === 1;
+            const portalId = container.dataset.portalId;
+            const formId = container.dataset.formId;
+            if (portalId && formId) {
+                const options = {
+                    portalId: portalId,
+                    formId: formId,
+                    target: '#' + container.id,
+                    onFormReady: function () {
+                        const messageElement = document.querySelector("#hubspot-form-message");
+                        messageElement.parentElement.removeChild(messageElement);
+                    }
+                };
+                if (disableCss) {
+                    options.css = '';
+                }
+                hbspt.forms.create(options);
+            } else {
+                console.log('Please validate your HubSpot Portal-ID and Form-ID.')
+            }
+        });
+    });
+
+})();
