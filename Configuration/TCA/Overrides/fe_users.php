3abe4ba82ca95ba5a1d3ccf63c483c7b6de5bcbb
mabolek <mathias@lilio.com>
2020-10-08 14:35:23 +0200

    [TASK] Added fe_users fields to TCA

---
+++ Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the package t3g/hubspot.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+call_user_func(function (string $extensionName, string $table) {
+    $columns = [
+        'hubspot_id' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
+        'hubspot_sync_timestamp' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ]
+    ];
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $columns);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        $table,
+        '--div--;LLL:EXT:' . $extensionName . '/Resources/Private/Language/locallang_db.xlf:fe_users.tab_label,' . implode(',', array_keys($columns))
+    );
+}, 'hubspot', 'fe_users');
