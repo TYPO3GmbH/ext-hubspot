86b0182a05a2e52aeb2995c860d16cd6e87ce5e4
mabolek <mathias@lilio.com>
2020-10-16 14:13:07 +0200

    [TASK] Added CompatibilityUtility
    Miscellaneous functions relating to compatibility with different TYPO3 versions

---
+++ Classes/Utility/CompatibilityUtility.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the package t3g/hubspot.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\Hubspot\Utility;
+
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
+/**
+ * Miscellaneous functions relating to compatibility with different TYPO3 versions
+ *
+ * @extensionScannerIgnoreFile
+ */
+class CompatibilityUtility
+{
+    /**
+     * Returns true if the current TYPO3 version is less than $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsLessThan($version)
+    {
+        return self::getTypo3VersionInteger() < VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is less than or equal to $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsLessThanOrEqualTo($version)
+    {
+        return self::getTypo3VersionInteger() <= VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is greater than $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsGreaterThan($version)
+    {
+        return self::getTypo3VersionInteger() > VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns true if the current TYPO3 version is greater than or equal to $version
+     *
+     * @param string $version
+     * @return bool
+     */
+    public static function typo3VersionIsGreaterThanOrEqualTo($version)
+    {
+        return self::getTypo3VersionInteger() >= VersionNumberUtility::convertVersionNumberToInteger($version);
+    }
+
+    /**
+     * Returns the TYPO3 version as an integer
+     *
+     * @return int
+     */
+    public static function getTypo3VersionInteger()
+    {
+        return VersionNumberUtility::convertVersionNumberToInteger(VersionNumberUtility::getNumericTypo3Version());
+    }
+}
