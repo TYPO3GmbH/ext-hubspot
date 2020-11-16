16ba80ad8c8bf32b73c6ec8dcfb0f98c59f51542
mabolek <mathias@lilio.com>
2020-11-16 13:13:09 +0100

    [TASK] Basic HubspotContactRepository test class

---
+++ Tests/Unit/Repository/HubspotContactRepositoryTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+/*
+ * This file is part of the package t3g/hubspot.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace T3G\Hubspot\Tests\Unit\Repository;
+
+use T3G\Hubspot\Repository\HubspotContactRepository;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class HubspotContactRepositoryTest extends UnitTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function testConvertAssociativeArrayToHubspotProperties()
+    {
+
+    }
+}
