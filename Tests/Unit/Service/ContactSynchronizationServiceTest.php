00cb1722348c0c9feaad4ef64f2304e4b7b74aba
mabolek <mathias@lilio.com>
2020-11-17 13:57:22 +0100

    [TASK] Revert to onlyMethods in tests

--- Tests/Unit/Service/ContactSynchronizationServiceTest.php
+++ Tests/Unit/Service/ContactSynchronizationServiceTest.php
@@ -199,7 +199,7 @@ class ContactSynchronizationServiceTest extends UnitTestCase
         $frontendUserData['hubspot_id'] = 0;
 
         $subjectMock = $this->subjectMockBuilder
-            ->setMethods(['addFrontendUserToHubspot'])
+            ->onlyMethods(['addFrontendUserToHubspot'])
             ->getMock();
 
         $this->setConfigurationOnObject(
@@ -302,7 +302,7 @@ class ContactSynchronizationServiceTest extends UnitTestCase
             $this->createMock(Dispatcher::class)
         ]);
 
-        $this->subjectMockBuilder->setMethods([]);
+        $this->subjectMockBuilder->onlyMethods([]);
 
         $subjectMock = $this->subjectMockBuilder->getMock();
 
