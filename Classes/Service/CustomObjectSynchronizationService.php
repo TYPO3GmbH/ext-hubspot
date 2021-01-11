760a4dd02c52024d989f0044328d4b781f0be6fe
mabolek <mathias@lilio.com>
2021-01-11 11:12:49 +0100

    [TASK] Basic scaffolding

--- Classes/Service/CustomObjectSynchronizationService.php
+++ Classes/Service/CustomObjectSynchronizationService.php
@@ -18,6 +18,23 @@ class CustomObjectSynchronizationService
 {
     public function synchronize(int $defaultPid = null)
     {
+        // TODO: Read TypoScript configuration from tx_hubspot.settings.synchronizeCustomObjects
 
+        // TODO: Iterate through objects that have not yet been synched, then objects with changes
+
+        // TODO: Check if relation exists. How do we deal with relations that do not exist? Sync them in a later pass
+        // TODO: or wait with syncing? How do we deal with existing objects in Hubspot that have a relation that is
+        // TODO: different to what we have on the TYPO3 side? (Suggestion: we do not sync)
+
+        // TODO: Check if object exists in Hubspot using uniqueFields list (for example product type and serial number).
+        // TODO: This is the unique key for the Hubspot object when we don't yet have a Hubspot ID for it.
+
+        // TODO: Write object data to Hubspot
+
+        // TODO: Write relations to Hubspot
+
+        // TODO: Persist Hubspot unique id to database, creating a record in tx_hubspot_object_foreigntable_mm
+
+        // TODO: For later: Synchronize the other way, back to TYPO3.
     }
 }
